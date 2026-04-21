6a2c0e758bf04e3c79497ef120a87b1a8be7bc7c
Mukul Singla <mukulsingla1421@gmail.com>
2026-04-21 10:45:29 +0530

    Change API base URL to production server

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,4 @@
-const BASE_URL = 'http://localhost:5000/api';
-
+const BASE_URL = 'https://simvesto.onrender.com/api';
 const getAuthHeaders = () => {
   const token = localStorage.getItem('token');
   return {
